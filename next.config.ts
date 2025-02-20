4ba8b7cb8f635bc7e4f9eb50a579843542356a7b
Raman <raman@STM-0036.communityfibre.co.uk>
2025-02-20 17:19:35 +0000

    new commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export", // This enables static export
   basePath: "/product-description-generator", // Replace with your GitHub repo name
   reactStrictMode: true,
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
